9eceef66c8ae768ceef951cf040d2d41a237e9bc
Andrejfr <85566390+Andrejfr@users.noreply.github.com>
2021-09-11 14:42:51 +0300

    Add files via upload
    Петров и Фролов

--- Program.cs
+++ Program.cs
@@ -3,44 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
-namespace ConsoleApplication2
+namespace ConsolnoePrilogenie
 {
 	class Program
 	{
 		static void Main(string[] args)
-		{
+		{   
 			bool ff;
-			string[] len = File.ReadAllLines(@"C:\Users\047-19\Desktop\val.txt");
-			do
-			{
-				foreach (var text in len)
-				{
-					bool flag = true;
-					for (int i = 0; i < text.Length / 2; i++)
-					{
-						if (text[i] != text[text.Length - (i + 1)])
-						{
-							flag = false;
-							break;
-						}
-					}
-					if (flag)
-						Console.WriteLine("{0} является палиндромом", text);
-					else
-						Console.WriteLine("{0} не является палиндромом", text);
-				}
-					Console.WriteLine("Для продолжения нажмите (1)");
-					int q = Int32.Parse(Console.ReadLine());
-					if (q == 1) ff = true;
-					else ff = false;
-					Console.Clear();
-			}
-			while (ff == true);
+		
+			do {
+				Console.Write("Ввод:");
+				string text = Console.ReadLine();
+				int len = text.Length;
+				bool flag = true;
 
-				Console.ReadLine();
+				for (int i = 0; i < len / 2; i++)
+				{
+					if (text[i] != text[len - (i + 1)])
+					{
+						flag = false;
+						break;
+					}
+				}
+				if (flag)
+				{
+					Console.WriteLine("{0} является палиндромом", text);
+				}
+				else
+				{
+					Console.WriteLine("{0} не является палиндромом", text);
+				}
+				Console.WriteLine("Для продолжения нажмите (1)");
+				int q = Int32.Parse(Console.ReadLine());
+				if (q == 1) ff = true;
+				else ff = false;
+				Console.Clear();
+			}
+			while (ff==true);
 			
-	   }
+			Console.ReadLine();
+		}
 	}
-} 
+}
